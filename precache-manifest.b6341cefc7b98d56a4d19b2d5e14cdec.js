f99daa7d5313522812af14dd3e82c7683c903dff
Erin McGeever <mcgeevee@me.com>
2020-11-29 18:53:44 -0700

    Updates

---
+++ precache-manifest.b6341cefc7b98d56a4d19b2d5e14cdec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a88bfab352f3570ae59006f155493e2e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "aa354bdd0d2b36f1be60",
+    "url": "/portfolio/static/css/main.7780cebc.chunk.css"
+  },
+  {
+    "revision": "cf7de063c87db73510e3",
+    "url": "/portfolio/static/js/2.1755d377.chunk.js"
+  },
+  {
+    "revision": "fa24d2988fd9ee29f9cba16979460922",
+    "url": "/portfolio/static/js/2.1755d377.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa354bdd0d2b36f1be60",
+    "url": "/portfolio/static/js/main.fd56ba42.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  }
+]);
